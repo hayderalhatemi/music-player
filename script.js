01e1069a90bff69cff5717ace105cb92af5413e7
Hayder <hayder.alhatemi@gmail.com>
2024-08-15 00:05:07 +0300

    modifying

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById('uploadd').addEventListener('change', function(event) {
+document.getElementById('upload').addEventListener('change', function(event) {
     const file = event.target.files[0];
     if (file) {
-        const audioPlayer = document.getElementById('music');
+        const audioPlayer = document.getElementById('song');
         const fileURL = URL.createObjectURL(file);
         audioPlayer.src = fileURL;
         audioPlayer.style.display = 'block';
