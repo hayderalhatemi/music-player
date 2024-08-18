728f5d1ad2a37c3e7cf443c4fd9f1fdf2abb977b
Hayder <hayder.alhatemi@gmail.com>
2024-08-18 21:16:25 +0300

    modifying

--- script.js
+++ script.js
@@ -2,8 +2,28 @@ let playList = document.querySelector('.audio');
 let list = document.getElementById('list');
 let back = document.getElementById('back');
 let buttons = document.querySelector('.buttons');
+let songs = document.querySelectorAll('.audio audio');
 
 window.onload = function() {
+    document.getElementById('upload').addEventListener('change', function(event) {
+        const files = event.target.files;
+        const audioContainer = document.querySelector('.audio2');
+    
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const fileURL = URL.createObjectURL(file);
+            const fileName = document.createElement('p');
+            fileName.textContent = file.name;
+            fileName.style.fontWeight = 'bold';
+            const audioPlayer = document.createElement('audio');
+            audioPlayer.controls = true;
+            audioPlayer.src = fileURL;
+
+            audioContainer.appendChild(audioPlayer);
+            audioContainer.appendChild(fileName);
+        };
+    });
+
     playList.style.display = 'none';
     buttons.style.display = 'none';
 };
@@ -15,23 +35,4 @@ list.addEventListener ('click', function() {
 back.addEventListener('click', function() {
     playList.style.display = 'none';
     buttons.style.display = 'none';
-});
-
-
-document.getElementById('upload').addEventListener('change', function(event) {
-    const files = event.target.files;
-    const audioContainer = document.querySelector('.audio2');
-
-    for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        const fileURL = URL.createObjectURL(file);
-        const fileName = document.createElement('p');
-        fileName.textContent = file.name;
-        fileName.style.fontWeight = 'bold';
-        const audioPlayer = document.createElement('audio');
-        audioPlayer.controls = true;
-        audioPlayer.src = fileURL;
-        audioContainer.appendChild(audioPlayer);
-        audioContainer.appendChild(fileName);
-    };
 });
